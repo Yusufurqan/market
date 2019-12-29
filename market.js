b1f0614e1ad56e101d50543a61cdbaba93a6d162
Yusufurqan <yusufurqan@gmail.com>
2019-12-30 04:25:21 +0700

    finish

---
+++ market.js
@@ -0,0 +1,13 @@
+
+        
+        var tabel = [
+            ["Nasi Goreng", 15000],
+            ["Nasi Uduk", 12000],
+            ["Nasi Bebek", 15500],
+            ["Nasi Kebuli", 25000],
+            ["Nasi Gila", 17000],
+            ["Nasi Goreng Ijo", 18000],
+            ["Pecel Lele", 13000],
+            ["Pecel Ayam", 20000]
+            ];
+        console.log(tabel[1][0])
